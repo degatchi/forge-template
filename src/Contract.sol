4186295ecbf2533267817d109fe42c037b2a0724
lucus.eth <lucusra@protonmail.com>
2022-02-01 17:21:05 +1100

    update: license + base contract

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,5 +1,13 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+// --------------------------------------------------------------------------------------
+//
+// (c) NftAirDropper 01/02/2022 | SPDX-License-Identifier: AGPL-3.0-only
+//  Designed by, DeGatchi (https://github.com/DeGatchi).
+//
+// --------------------------------------------------------------------------------------
 
 contract Contract {
     string public greet = "Hello World!";
